nvlsianpu/mcuboot

boot: zephyr: migrate to new log subystem
The old log subsystem has been deprecated in Zephyr.
Migrate to the new subsystem to avoid compilation warnings.

In-place log processing is selected as it is required as MCUBoot is
one thread application.

Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>
Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>

boot/zephyr/flash_map_extended.c
@@ -15,6 +15,8 @@
 
 #include "bootutil/bootutil_log.h"
 
+LOG_MODULE_DECLARE(mcuboot);
+
 #if (!defined(CONFIG_XTENSA) && defined(DT_FLASH_DEV_NAME))
 #define FLASH_DEVICE_ID SOC_FLASH_0_ID
 #elif (defined(CONFIG_XTENSA) && defined(DT_SPI_NOR_DRV_NAME))

boot/zephyr/flash_map_legacy.c
@@ -49,6 +49,8 @@
 #define FLASH_AREA_IMAGE_SECTOR_SIZE FLASH_AREA_IMAGE_SCRATCH_SIZE
 #endif
 
+LOG_MODULE_DECLARE(mcuboot);
+
 /*
  * Lookup the sector map for a given flash area.  This should fill in
  * `ret` with all of the sectors in the area.  `*cnt` will be set to

boot/zephyr/main.c
@@ -40,6 +40,8 @@ const struct boot_uart_funcs boot_funcs = {
 };
 #endif
 
+LOG_MODULE_REGISTER(mcuboot);
+
 void os_heap_init(void);
 
 #if defined(CONFIG_ARM)

boot/zephyr/serial_adapter.c
@@ -25,6 +25,8 @@
 #error Zephyr UART console must been disabled if serial_adapter module is used.
 #endif
 
+LOG_MODULE_REGISTER(serial_adapter);
+
 /** @brief Console input representation
  *
  * This struct is used to represent an input line from a serial interface.

nrfconnect/sdk-mcuboot

boot/zephyr/flash_map_extended.c
@@ -15,6 +15,8 @@
 
 #include "bootutil/bootutil_log.h"
 
+MCUBOOT_LOG_MODULE_DECLARE(mcuboot);
+
 #if (!defined(CONFIG_XTENSA) && defined(DT_FLASH_DEV_NAME))
 #define FLASH_DEVICE_ID SOC_FLASH_0_ID
 #elif (defined(CONFIG_XTENSA) && defined(DT_SPI_NOR_DRV_NAME))

boot/zephyr/flash_map_legacy.c
@@ -49,6 +49,8 @@
 #define FLASH_AREA_IMAGE_SECTOR_SIZE FLASH_AREA_IMAGE_SCRATCH_SIZE
 #endif
 
+MCUBOOT_LOG_MODULE_DECLARE(mcuboot);
+
 /*
  * Lookup the sector map for a given flash area.  This should fill in
  * `ret` with all of the sectors in the area.  `*cnt` will be set to

boot/zephyr/main.c
@@ -40,6 +40,8 @@ const struct boot_uart_funcs boot_funcs = {
 };
 #endif
 
+MCUBOOT_LOG_MODULE_REGISTER(mcuboot);
+
 void os_heap_init(void);
 
 #if defined(CONFIG_ARM)

boot/zephyr/serial_adapter.c
@@ -25,6 +25,8 @@
 #error Zephyr UART console must been disabled if serial_adapter module is used.
 #endif
 
+MCUBOOT_LOG_MODULE_REGISTER(serial_adapter);
+
 /** @brief Console input representation
  *
  * This struct is used to represent an input line from a serial interface.