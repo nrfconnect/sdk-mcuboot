5f71e809d526f568b2b657d9f7f21ccf8e7d2f89
Sigvart Hovland <sigvart.m@gmail.com>
2019-02-14 13:20:34 +0100

    [nrf noup] boot: bootutil: Add shared crypto for ECDSA and SHA
    * Add functions for ecdsa_verify_secp256r1 and sha256 to use the shared
    crypto API
    * Add Kconfig and CMake variables for selecting shared crypto when using
    ecdsa
    * Add custom section to project for placing the API section in the
    correct location in flash
    * Add kconfig fragment for using external crypto
    
    Signed-off-by: Sigvart Hovland <sigvart.m@gmail.com>
    Signed-off-by: Martí Bolívar <marti.bolivar@nordicsemi.no>
    Signed-off-by: Emil Obalski <emil.obalski@nordicsemi.no>
    Signed-off-by: Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
    Signed-off-by: Håkon Øye Amundsen <haakon.amundsen@nordicsemi.no>
    Signed-off-by: Ioannis Glaropoulos <Ioannis.Glaropoulos@nordicsemi.no>
    Signed-off-by: Trond Einar Snekvik <Trond.Einar.Snekvik@nordicsemi.no>
    Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>
    Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>
    (cherry picked from commit e74154072075f0b5e0aaf573b832ea222ef443a3)
    (cherry picked from commit 6804ab2617b62cef534300733e4fb916fbfeb650)
    (cherry picked from commit e912bb25f2fc265c1335abbc388cac8feecfc662)
    (cherry picked from commit 90acd027b7d01fbc58833c4a858c2ba22c15803e)

--- boot/bootutil/include/bootutil/crypto/sha256.h
+++ boot/bootutil/include/bootutil/crypto/sha256.h
@@ -30,6 +30,7 @@
 
 #if (defined(MCUBOOT_USE_PSA_OR_MBED_TLS) + \
      defined(MCUBOOT_USE_TINYCRYPT) + \
+     defined(MCUBOOT_USE_NRF_EXTERNAL_CRYPTO) + \
      defined(MCUBOOT_USE_CC310)) != 1
     #error "One crypto backend must be defined: either CC310/MBED_TLS/TINYCRYPT/PSA_CRYPTO"
 #endif
@@ -187,6 +188,37 @@ static inline int bootutil_sha256_finish(bootutil_sha256_context *ctx,
     return 0;
 }
 #endif /* MCUBOOT_USE_CC310 */
+
+#if defined(MCUBOOT_USE_NRF_EXTERNAL_CRYPTO)
+
+#include <bl_crypto.h>
+
+typedef bl_sha256_ctx_t bootutil_sha256_context;
+
+static inline void bootutil_sha256_init(bootutil_sha256_context *ctx)
+{
+    bl_sha256_init(ctx);
+}
+
+static inline void bootutil_sha256_drop(bootutil_sha256_context *ctx)
+{
+    (void)ctx;
+}
+
+static inline int bootutil_sha256_update(bootutil_sha256_context *ctx,
+                                          const void *data,
+                                          uint32_t data_len)
+{
+    return bl_sha256_update(ctx, data, data_len);
+}
+
+static inline int bootutil_sha256_finish(bootutil_sha256_context *ctx,
+                                          uint8_t *output)
+{
+    bl_sha256_finalize(ctx, output);
+    return 0;
+}
+#endif /* MCUBOOT_USE_NRF_EXTERNAL_CRYPTO */
 
 #ifdef __cplusplus
 }
