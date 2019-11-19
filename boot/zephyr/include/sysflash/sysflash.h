5dae909b6da3c13f51b01881268ab93260607d65
Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
2019-11-19 14:14:05 +0100

    Revert "[nrf noup] cmake: multi-image + partition manager"
    This reverts commit 0fa1f04f8b43bcce1fe84e314c194f2ff6d28219.
    
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>

--- boot/zephyr/include/sysflash/sysflash.h
+++ boot/zephyr/include/sysflash/sysflash.h
@@ -3,15 +3,6 @@
 #ifndef __SYSFLASH_H__
 #define __SYSFLASH_H__
 
-#if USE_PARTITION_MANAGER
-#include <pm_config.h>
-
-#define FLASH_AREA_IMAGE_PRIMARY    PM_MCUBOOT_PRIMARY_ID
-#define FLASH_AREA_IMAGE_SECONDARY  PM_MCUBOOT_SECONDARY_ID
-#define FLASH_AREA_IMAGE_SCRATCH    PM_MCUBOOT_SCRATCH_ID
-
-#else
-
 #include <generated_dts_board.h>
 #include <mcuboot_config/mcuboot_config.h>
 
@@ -47,7 +38,5 @@
 #endif
 
 #define FLASH_AREA_IMAGE_SCRATCH    DT_FLASH_AREA_IMAGE_SCRATCH_ID
-#endif /* USE_PARTITION_MANAGER */
-
 
 #endif /* __SYSFLASH_H__ */
