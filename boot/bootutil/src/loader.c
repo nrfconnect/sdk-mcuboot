4ce3844d5fb9a1b0f90b2f95461f23cdba3e9080
Sigvart Hovland <sigvart.hovland@nordicsemi.no>
2021-03-30 22:45:17 +0200

    [nrf noup] loader: work-around for multi-image builds
    Seems multi-image dependencies are not supported for multi-image in
    NCS yet. This is a workaround which reverts some lines to restore
    previous MCUboot behavior, so that Immutable bootloader + MCUBoot type
    builds will work.
    
    Ref. NCSDK-8681
    
    Signed-off-by: Sigvart Hovland <sigvart.hovland@nordicsemi.no>
    Signed-off-by: Torsten Rasmussen <Torsten.Rasmussen@nordicsemi.no>
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>
    (cherry picked from commit e02925a085a107284cb1329742b43859d40afb37)
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- boot/bootutil/src/loader.c
+++ boot/bootutil/src/loader.c
@@ -402,7 +402,7 @@ boot_verify_dependencies(struct boot_loader_state *state)
         if (rc == 0) {
             /* All dependencies've been satisfied, continue with next image. */
             BOOT_CURR_IMG(state)++;
-        } else {
+        } else if (rc == BOOT_EBADIMAGE) {
             /* Cannot upgrade due to non-met dependencies, so disable all
              * image upgrades.
              */
@@ -411,7 +411,10 @@ boot_verify_dependencies(struct boot_loader_state *state)
                 BOOT_SWAP_TYPE(state) = BOOT_SWAP_TYPE_NONE;
             }
             break;
-        }
+        } else {
+		/* Other error happened, images are inconsistent */
+		return rc;
+	}
     }
     return rc;
 }
@@ -1751,7 +1754,6 @@ boot_swap_image(struct boot_loader_state *state, struct boot_status *bs)
     return 0;
 }
 #endif
-
 
 /**
  * Performs a clean (not aborted) image update.
