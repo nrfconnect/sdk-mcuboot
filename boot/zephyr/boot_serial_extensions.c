77c147a0b4baf94e78fcd5d5edd1511f92cb3f5d
Gerard Marull-Paretas <gerard@teslabs.com>
2022-03-03 17:02:07 +0100

    Revert "[nrf fromlist] zephyr/boot_serial_extension: us BOOT_LOG instead of LOG_"
    This reverts commit 986e8184738a5cd599189a0846829d752d15bf93.

--- boot/zephyr/boot_serial_extensions.c
+++ boot/zephyr/boot_serial_extensions.c
@@ -19,7 +19,7 @@
 #include "bootutil/bootutil_public.h"
 #include "bootutil/boot_hooks.h"
 
-BOOT_LOG_MODULE_DECLARE(mcuboot);
+MCUBOOT_LOG_MODULE_DECLARE(mcuboot);
 
 #ifdef CONFIG_BOOT_MGMT_CUSTOM_STORAGE_ERASE
 static int bs_custom_storage_erase(cbor_state_t *cs)
@@ -31,11 +31,11 @@ static int bs_custom_storage_erase(cbor_state_t *cs)
     rc = flash_area_open(FLASH_AREA_ID(storage), &fa);
 
     if (rc < 0) {
-        BOOT_LOG_ERR("failed to open flash area");
+        LOG_ERR("failed to open flash area");
     } else {
         rc = flash_area_erase(fa, 0, FLASH_AREA_SIZE(storage));
         if (rc < 0) {
-            BOOT_LOG_ERR("failed to erase flash area");
+            LOG_ERR("failed to erase flash area");
         }
         flash_area_close(fa);
     }
