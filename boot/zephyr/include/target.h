9934ddb7f133e74cc66d312163e9faa08fc0fb43
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2022-08-03 14:04:54 +0100

    Revert "[nrf noup] treewide: add NCS partition manager support"
    This reverts commit 7f6aa0794588f7d42ae32e204fc581c644698bab.
    
    Signed-off-by: Jamie McCrae <jamie.mccrae@nordicsemi.no>

--- boot/zephyr/include/target.h
+++ boot/zephyr/include/target.h
@@ -8,8 +8,6 @@
 #ifndef H_TARGETS_TARGET_
 #define H_TARGETS_TARGET_
 
-#ifndef USE_PARTITION_MANAGER
-
 #if defined(MCUBOOT_TARGET_CONFIG)
 /*
  * Target-specific definitions are permitted in legacy cases that
@@ -49,7 +47,5 @@
                                      !(FLASH_AREA_LABEL_EXISTS(image_3)))
 #error "Target support is incomplete; cannot build mcuboot."
 #endif
-
-#endif /* ifndef USE_PARTITION_MANAGER */
 
 #endif /* H_TARGETS_TARGET_ */
