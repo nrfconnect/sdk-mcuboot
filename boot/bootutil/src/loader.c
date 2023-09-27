420ac794db054e9a3bae31c6627d5b08920de0ed
Sigvart Hovland <sigvart.hovland@nordicsemi.no>
2023-09-27 15:18:04 +0200

    [nrf noup] loader: Add firmware version check downgrade prevention
    For nRF53, the only existing version number metadata is stored in the
    `firmware_info` structure in the network core. This utilizes PCD to read
    out the version number and compares it against the version number found
    in the secondary slot for the network core.
    
    Ref. NCSDK-21379
    
    Signed-off-by: Sigvart Hovland <sigvart.hovland@nordicsemi.no>
    (cherry picked from commit 42e8551d6bdcb50388ab7aac51d44aa12cf8ef55)

--- boot/bootutil/src/loader.c
+++ boot/bootutil/src/loader.c
@@ -52,6 +52,10 @@
 
 #if defined(CONFIG_SOC_NRF5340_CPUAPP) && defined(PM_CPUNET_B0N_ADDRESS) && defined(CONFIG_PCD_APP)
 #include <dfu/pcd.h>
+#ifdef CONFIG_PCD_READ_NETCORE_APP_VERSION
+#include <fw_info_bare.h>
+int pcd_version_cmp_net(const struct flash_area *fap, struct image_header *hdr);
+#endif
 #endif
 
 #ifdef MCUBOOT_ENC_IMAGES
@@ -1159,9 +1163,21 @@ boot_validate_slot(struct boot_loader_state *state, int slot,
         int rc;
 
         /* Check if version of secondary slot is sufficient */
-        rc = boot_version_cmp(
-                &boot_img_hdr(state, BOOT_SECONDARY_SLOT)->ih_ver,
-                &boot_img_hdr(state, BOOT_PRIMARY_SLOT)->ih_ver);
+
+#if defined(CONFIG_SOC_NRF5340_CPUAPP) && defined(CONFIG_NRF53_MULTI_IMAGE_UPDATE) \
+    && defined(CONFIG_PCD_APP) && defined(CONFIG_PCD_READ_NETCORE_APP_VERSION)
+        if (BOOT_CURR_IMG(state) == 1) {
+            rc = pcd_version_cmp_net(fap, boot_img_hdr(state, BOOT_SECONDARY_SLOT));
+        } else {
+             rc = boot_version_cmp(
+                                 &boot_img_hdr(state, BOOT_SECONDARY_SLOT)->ih_ver,
+                                 &boot_img_hdr(state, BOOT_PRIMARY_SLOT)->ih_ver);
+        }
+#else
+	rc = boot_version_cmp(
+			&boot_img_hdr(state, BOOT_SECONDARY_SLOT)->ih_ver,
+			&boot_img_hdr(state, BOOT_PRIMARY_SLOT)->ih_ver);
+#endif
         if (rc < 0 && boot_check_header_erased(state, BOOT_PRIMARY_SLOT)) {
             BOOT_LOG_ERR("insufficient version in secondary slot");
             boot_scramble_slot(fap, slot);
