bddbcf35211154c55ae8beec44de15d5dac9f008
Dominik Ermel <dominik.ermel@nordicsemi.no>
2023-12-11 19:01:42 +0000

    Revert "[nrf noup] boot: zephyr: Add ifdef protection for RAM locking"
    This reverts commit 52997e197541cf12908b4f341fbe0a0601bbc9f7.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- boot/zephyr/main.c
+++ boot/zephyr/main.c
@@ -726,7 +726,7 @@ int main(void)
             ;
     }
 
-#if defined(CONFIG_SOC_NRF5340_CPUAPP) && defined(PM_CPUNET_B0N_ADDRESS) && defined(CONFIG_PCD_APP)
+#if defined(CONFIG_SOC_NRF5340_CPUAPP) && defined(PM_CPUNET_B0N_ADDRESS)
     pcd_lock_ram();
 #endif
 #endif /* USE_PARTITION_MANAGER && CONFIG_FPROTECT */
