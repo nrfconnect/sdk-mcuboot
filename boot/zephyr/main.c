2ca556546a8b0fdedeea7a9c63048d347979e6da
Robert Lubos <robert.lubos@nordicsemi.no>
2024-02-06 10:18:01 +0100

    Revert "[nrf noup] boot: nrf53-specific customizations"
    This reverts commit 2bbd3b115b34a68908d6e8cde057292de276c552.
    
    Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>

--- boot/zephyr/main.c
+++ boot/zephyr/main.c
@@ -94,10 +94,6 @@ const struct boot_uart_funcs boot_funcs = {
 #include <zephyr/drivers/hwinfo.h>
 #endif
 
-#if defined(CONFIG_SOC_NRF5340_CPUAPP) && defined(PM_CPUNET_B0N_ADDRESS)
-#include <dfu/pcd.h>
-#endif
-
 /* CONFIG_LOG_MINIMAL is the legacy Kconfig property,
  * replaced by CONFIG_LOG_MODE_MINIMAL.
  */
@@ -727,9 +723,6 @@ int main(void)
             ;
     }
 
-#if defined(CONFIG_SOC_NRF5340_CPUAPP) && defined(PM_CPUNET_B0N_ADDRESS)
-    pcd_lock_ram();
-#endif
 #endif /* USE_PARTITION_MANAGER && CONFIG_FPROTECT */
 
     ZEPHYR_BOOT_LOG_STOP();
