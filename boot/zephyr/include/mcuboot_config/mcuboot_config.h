67f50537c5e46aa363efdf8aa211c074d5bc78f2
Mateusz Michalek <mateusz.michalek@nordicsemi.no>
2024-05-20 08:47:02 +0200

    boot: SHA512 verification
    adds TLV and Kconfig to decouple verification from
    other options.
    
    Signed-off-by: Mateusz Michalek <mateusz.michalek@nordicsemi.no>

--- boot/zephyr/include/mcuboot_config/mcuboot_config.h
+++ boot/zephyr/include/mcuboot_config/mcuboot_config.h
@@ -44,6 +44,11 @@
 #define MCUBOOT_USE_PSA_CRYPTO
 #elif defined(CONFIG_BOOT_USE_NRF_EXTERNAL_CRYPTO)
 #define MCUBOOT_USE_NRF_EXTERNAL_CRYPTO
+#elif defined(CONFIG_BOOT_USE_NRF_EXTERNAL_CRYPTO)
+#define MCUBOOT_USE_NRF_EXTERNAL_CRYPTO
+
+#ifdef CONFIG_BOOT_SIGNATURE_TYPE_NONE_SHA512
+#define MCUBOOT_SHA512
 #endif
 
 /* Zephyr, regardless of C library used, provides snprintf */
