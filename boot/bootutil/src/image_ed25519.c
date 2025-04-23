mcu-tools/mcuboot

bootutil: PSA implementation of x25519 and ed25519 verification
The commit provides implementation of image verification with
ed25519 and encryption/decryption support where random key
is encrypted using x25519.

Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

boot/bootutil/src/image_ed25519.c
@@ -85,7 +85,7 @@ bootutil_verify(uint8_t *buf, uint32_t blen,
     uint8_t *pubkey;
     uint8_t *end;
 
-    if (slen != EDDSA_SIGNATURE_LENGTH) {
+    if (hlen != IMAGE_HASH_SIZE || slen != EDDSA_SIGNATURE_LENGTH) {
         FIH_SET(fih_rc, FIH_FAILURE);
         goto out;
     }

nrfconnect/sdk-mcuboot

(cherry picked from commit 615a9dffd03e6ebcdd1bcc58a67cc92e5eafea7b)
(cherry picked from commit a671768dd0973a737db7b2737697f812375bd45b)

boot/bootutil/src/image_ed25519.c
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2019 JUUL Labs
  * Copyright (c) 2021-2023 Arm Limited
+ * Copyright (c) 2025 Nordic Semiconductor ASA
  */
 
 #include <string.h>
@@ -85,7 +86,7 @@ bootutil_verify(uint8_t *buf, uint32_t blen,
     uint8_t *pubkey;
     uint8_t *end;
 
-    if (slen != EDDSA_SIGNATURE_LENGTH) {
+    if (blen != IMAGE_HASH_SIZE || slen != EDDSA_SIGNATURE_LENGTH) {
         FIH_SET(fih_rc, FIH_FAILURE);
         goto out;
     }