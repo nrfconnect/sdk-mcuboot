d69621e3032f03ddf462eb3a9d2df5af03955898
Artur Hadasz <artur.hadasz@nordicsemi.no>
2025-07-24 13:31:00 +0200

    [nrf noup] Added BOOT_SIGNATURE_USING_ITS for ecdsa configuration
    This configuration has the purpose of using keys provisioned
    to the internal trusted storage (ITS). It makes use of the
    already existing parts of code for MCUBOOT_BUILTIN_KEY
    
    Signed-off-by: Artur Hadasz <artur.hadasz@nordicsemi.no>
    (cherry picked from commit 68379845127b83191fd3d06591e084f6a532a9a5)

--- boot/bootutil/src/image_validate.c
+++ boot/bootutil/src/image_validate.c
@@ -514,7 +514,8 @@ bootutil_img_validate(struct boot_loader_state *state,
 #endif
                      )
 {
-#if (defined(EXPECTED_KEY_TLV) && defined(MCUBOOT_HW_KEY)) || defined(MCUBOOT_HW_ROLLBACK_PROT) || defined(MCUBOOT_DECOMPRESS_IMAGES)
+#if (defined(EXPECTED_KEY_TLV) && defined(MCUBOOT_HW_KEY)) || defined(MCUBOOT_HW_ROLLBACK_PROT) || defined(MCUBOOT_DECOMPRESS_IMAGES) \
+    || defined(MCUBOOT_BUILTIN_KEY)
     int image_index = (state == NULL ? 0 : BOOT_CURR_IMG(state));
 #endif
     uint32_t off;
