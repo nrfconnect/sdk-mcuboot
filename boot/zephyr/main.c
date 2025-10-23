df47c7c13b2c84b6ea908e2a3b83b02c0c018dfa
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2025-10-23 19:17:55 +0200

    Revert "[nrf noup] zephyr: clean peripherals state before boot"
    This reverts commit d6a003b8ebce039fb9bff4e8571cc59f159aa4a4.
    
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

--- boot/zephyr/main.c
+++ boot/zephyr/main.c
@@ -149,10 +149,6 @@ K_SEM_DEFINE(boot_log_sem, 1, 1);
 #include <pm_config.h>
 #endif
 
-#if CONFIG_MCUBOOT_NRF_CLEANUP_PERIPHERAL
-#include <nrf_cleanup.h>
-#endif
-
 BOOT_LOG_MODULE_REGISTER(mcuboot);
 
 void os_heap_init(void);
@@ -227,9 +223,7 @@ static void do_boot(struct boot_rsp *rsp)
     }
 #endif
 #endif
-#if CONFIG_MCUBOOT_NRF_CLEANUP_PERIPHERAL
-    nrf_cleanup_peripheral();
-#endif
+
 #if CONFIG_MCUBOOT_CLEANUP_ARM_CORE
     cleanup_arm_interrupts(); /* Disable and acknowledge all interrupts */
 
