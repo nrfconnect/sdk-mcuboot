295aabf4ce42ca3ae11661255604449ac31397b1
Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>
2025-10-23 19:17:55 +0200

    Revert "[nrf noup] loader: Do not check reset vector for XIP image"
    This reverts commit 99de74982aaef827e0fb0e8179f2f74d02230d8a.
    
    Signed-off-by: Tomasz Chyrowicz <tomasz.chyrowicz@nordicsemi.no>

--- boot/bootutil/src/loader.c
+++ boot/bootutil/src/loader.c
@@ -973,18 +973,8 @@ check_validity:
      * overwriting an application written to the incorrect slot.
      * This feature is only supported by ARM platforms.
      */
-#if MCUBOOT_IMAGE_NUMBER >= 3
-    /* Currently the MCUboot can be configured for up to 3 image, where image number 2 is
-     * designated for XIP, where it is the second part of image stored in slots of image
-     * 0. This part of image is not bootable, as the XIP setup is done by the app in
-     * image 0 slot, and it does not carry the reset vector.
-     */
-    if (fap == state->imgs[2][BOOT_SECONDARY_SLOT].area) {
-        goto out;
-    }
-#endif
-    if (fap == BOOT_IMG_AREA(state, BOOT_SECONDARY_SLOT)) {
-        const struct flash_area *pri_fa = BOOT_IMG_AREA(state, BOOT_PRIMARY_SLOT);
+    if (fap == BOOT_IMG_AREA(state, BOOT_SLOT_SECONDARY)) {
+        const struct flash_area *pri_fa = BOOT_IMG_AREA(state, BOOT_SLOT_PRIMARY);
         struct image_header *secondary_hdr = boot_img_hdr(state, slot);
         uint32_t reset_value = 0;
         uint32_t reset_addr = secondary_hdr->ih_hdr_size + sizeof(reset_value);
