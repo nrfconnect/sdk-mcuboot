d4671061b41ad03fe62900fb504d4c26fb85fc3c
Dominik Ermel <dominik.ermel@nordicsemi.no>
2026-02-12 17:46:17 +0000

    [nrf noup] zephyr: All partition definitions needed for b0 builds
    Add mcuboot_s0 and mcuboot_s1 used by second stage bootloader.
    
    Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>

--- boot/zephyr/include/sysflash/sysflash.h
+++ boot/zephyr/include/sysflash/sysflash.h
@@ -56,7 +56,7 @@
 #endif
 
 /* Header size within MCUboot bootable application image */
-#define APP_IMAGE_HEADER_SIZE CONFIG_PM_PARTITION_SIZE_MCUBOOT_PAD
+#define PROTECTED_REGION_START_SKIP PM_MCUBOOT_PAD_SIZE
 
 #ifdef SECOND_STAGE_MCUBOOT_RUNNING_FROM_S0
 #define SECOND_STAGE_ACTIVE_MCUBOOT_OFFSET     PM_S0_OFFSET
@@ -92,9 +92,9 @@
  * it does have header added, but it is not used by the NSIB.
  */
 #define PROTECTED_REGION_START \
-    (SECOND_STAGE_ACTIVE_MCUBOOT_OFFSET + APP_IMAGE_HEADER_SIZE)
+    (SECOND_STAGE_ACTIVE_MCUBOOT_OFFSET + PROTECTED_REGION_START_SKIP)
 #define PROTECTED_REGION_SIZE  \
-    (SECOND_STAGE_ACTIVE_MCUBOOT_SIZE - APP_IMAGE_HEADER_SIZE)
+    (SECOND_STAGE_ACTIVE_MCUBOOT_SIZE - PROTECTED_REGION_START_SKIP)
 #endif /* CONFIG_NCS_MCUBOOT_DISABLE_SELF_RWX */
 
 #else /* MCUBOOT_IS_SECOND_STAGE */
@@ -145,34 +145,101 @@
 
 #if !defined(CONFIG_SINGLE_APPLICATION_SLOT) && !defined(CONFIG_MCUBOOT_BOOTLOADER_MODE_SINGLE_APP)
 
-/* Each pair of slots is separated by , and there is no terminating character */
-#define FLASH_AREA_IMAGE_0_SLOTS    slot0_partition, slot1_partition
-#define FLASH_AREA_IMAGE_1_SLOTS    slot2_partition, slot3_partition
-#define FLASH_AREA_IMAGE_2_SLOTS    slot4_partition, slot5_partition
+#if MCUBOOT_IS_SECOND_STAGE
 
-#if (MCUBOOT_IMAGE_NUMBER == 1)
-#define ALL_AVAILABLE_SLOTS FLASH_AREA_IMAGE_0_SLOTS
-#elif (MCUBOOT_IMAGE_NUMBER == 2)
-#define ALL_AVAILABLE_SLOTS FLASH_AREA_IMAGE_0_SLOTS, \
-                            FLASH_AREA_IMAGE_1_SLOTS
-#elif (MCUBOOT_IMAGE_NUMBER == 3)
-#define ALL_AVAILABLE_SLOTS FLASH_AREA_IMAGE_0_SLOTS, \
-                            FLASH_AREA_IMAGE_1_SLOTS, \
-                            FLASH_AREA_IMAGE_2_SLOTS
+/* Defined when building MCUboot for S1 */
+#ifdef CONFIG_NCS_IS_VARIANT_IMAGE
+#define SECOND_STAGE_MCUBOOT_RUNNING_FROM_S1
+#define SECOND_STAGE_ACTIVE_PARTITION           mcuboot_s1
+#define SECOND_STAGE_INACTIVE_PARTITION         mcuboot_s0
+#else
+#define SECOND_STAGE_MCUBOOT_RUNNING_FROM_S0
+#define SECOND_STAGE_ACTIVE_PARTITION           mcuboot_s0
+#define SECOND_STAGE_INACTIVE_PARTITION         mcuboot_s1
 #endif
+
+/* Header size within MCUboot bootable application image */
+#define PROTECTED_REGION_START_SKIP CONFIG_NCS_MCUBOOT_DISABLE_SELF_RWX_SKIP_SIZE
+
+#define SECOND_STAGE_ACTIVE_MCUBOOT_OFFSET      \
+    FIXED_PARTITION_OFFSET(SECOND_STAGE_ACTIVE_PARTITION)
+
+#define SECOND_STAGE_ACTIVE_MCUBOOT_SIZE        \
+    FIXED_PARTITION_SIZE(SECOND_STAGE_ACTIVE_PARTITION)
+
+#define SECOND_STAGE_ACTIVE_MCUBOOT_ID          \
+    FIXED_PARTITION_ID(SECOND_STAGE_ACTIVE_PARTITION)
+
+#define SECOND_STAGE_INACTIVE_MCUBOOT_OFFSET      \
+    FIXED_PARTITION_OFFSET(SECOND_STAGE_INACTIVE_PARTITION)
+
+#define SECOND_STAGE_INACTIVE_MCUBOOT_SIZE        \
+    FIXED_PARTITION_SIZE(SECOND_STAGE_INACTIVE_PARTITION)
+
+#define SECOND_STAGE_INACTIVE_MCUBOOT_ID          \
+    FIXED_PARTITION_ID(SECOND_STAGE_INACTIVE_PARTITION)
+
+/* FPROTECT region covers both S0 and S1 slots. Assumption here is
+ * that they precede PRIMARY application image partition, in flash
+ * layout.
+ */
+#ifdef CONFIG_FPROTECT
+#define FPROTECT_REGION_OFFSET  FIXED_PARTITION_OFFSET(mcuboot_s0)
+#define FPROTECT_REGION_SIZE    \
+    (FIXED_PARTITION_SIZE(mcuboot_s0) + FIXED_PARTITION_SIZE(mcuboot_s1))
+#endif
+
+/* RWX protection regions: the currently executing MCUboot is protecting itself */
+#if CONFIG_NCS_MCUBOOT_DISABLE_SELF_RWX
+/* Note: The CONFIG_ROM_START_OFFSET is removed to save RWX bits on space
+ * that do not require protection. When MCUboot is Second Stage bootloader
+ * it does have header added, but it is not used by the NSIB.
+ */
+#define PROTECTED_REGION_START \
+    (SECOND_STAGE_ACTIVE_MCUBOOT_OFFSET + CONFIG_ROM_START_OFFSET)
+#define PROTECTED_REGION_SIZE  \
+    (SECOND_STAGE_ACTIVE_MCUBOOT_SIZE - CONFIG_ROM_START_OFFSET)
+#endif /* CONFIG_NCS_MCUBOOT_DISABLE_SELF_RWX */
+
+/* Normally MCUboot needs one pair of slots per image it is able to process,
+ * to be able to pre-allocate proper number of support objects;
+ * same number is used to define number of slot pairs, one per image.
+ *
+ * Exception here is MCUBoot that serves as second stage image, because
+ * in this configuration it brings one own pair and requires one set
+ * of objects for it, so we set it one less to than number of images,
+ * to bring one less default pair.
+ */
+#define MCUBOOT_NEEDED_SLOT_PARIRS      (MCUBOOT_IMAGE_NUMBER - 1)
+
+#else /* MCUBOOT_IS_SECOND_STAGE != 0 */
+
+/* We need one pair of slots per image */
+#define MCUBOOT_NEEDED_SLOT_PARIRS      MCUBOOT_IMAGE_NUMBER
+
+#endif /* MCUBOOT_IS_SECOND_STAGE */
 
 static inline uint32_t __flash_area_ids_for_slot(int img, int slot)
 {
     static const int all_slots[] = {
-        FOR_EACH_NONEMPTY_TERM(FIXED_PARTITION_ID, (,), ALL_AVAILABLE_SLOTS)
+        FIXED_PARTITION_ID(slot0_partition), FIXED_PARTITION_ID(slot0_partition),
+#if MCUBOOT_NEEDED_SLOT_PARIRS > 1
+        FIXED_PARTITION_ID(slot2_partition), FIXED_PARTITION_ID(slot3_partition),
+#endif
+#if MCUBOOT_NEEDED_SLOT_PARIRS > 2
+        FIXED_PARTITION_ID(slot4_partition), FIXED_PARTITION_ID(slot5_partition),
+#endif
+#if MCUBOOT_IS_SECOND_STAGE == 1
+/* MCUboot as a second stage bootloader brings in two additional slots;
+ * one slot is slot that it can update itself to and the other is source
+ * of update. At this point source slot is, by default, secondary slot
+ * of primary image.
+ */
+        SECOND_STAGE_INACTIVE_MCUBOOT_ID, FIXED_PARTITION_ID(slot1_partition)
+#endif
     };
     return all_slots[img * 2 + slot];
 };
-
-#undef FLASH_AREA_IMAGE_0_SLOTS
-#undef FLASH_AREA_IMAGE_1_SLOTS
-#undef FLASH_AREA_IMAGE_2_SLOTS
-#undef ALL_AVAILABLE_SLOTS
 
 #define FLASH_AREA_IMAGE_PRIMARY(x) __flash_area_ids_for_slot(x, 0)
 #define FLASH_AREA_IMAGE_SECONDARY(x) __flash_area_ids_for_slot(x, 1)
@@ -188,6 +255,8 @@ static inline uint32_t __flash_area_ids_for_slot(int img, int slot)
 
 #endif /* CONFIG_SINGLE_APPLICATION_SLOT */
 
+/* Here are protection ranges for primary stage MCUboot */
+#if MCUBOOT_IS_SECOND_STAGE == 0
 /* Protecting MCUboot partition */
 #ifdef CONFIG_FPROTECT
 #define FPROTECT_REGION_OFFSET  FIXED_PARTITION_OFFSET(boot_partition)
@@ -199,5 +268,6 @@ static inline uint32_t __flash_area_ids_for_slot(int img, int slot)
 #define PROTECTED_REGION_START  FIXED_PARTITION_OFFSET(boot_partition)
 #define PROTECTED_REGION_SIZE   FIXED_PARTITION_SIZE(boot_partition)
 #endif /* CONFIG_NCS_MCUBOOT_DISABLE_SELF_RWX */
+#endif
 
 #endif /* __SYSFLASH_H__ */
